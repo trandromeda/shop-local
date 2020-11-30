d3da09ab8f45f2b129cb9da3324fb29c0e550650
Andy Tran <transient.tran@gmail.com>
2020-11-30 02:33:03 -0500

    make app prettier for publishing

--- src/shops/components/shop/Shop.tsx
+++ src/shops/components/shop/Shop.tsx
@@ -25,10 +25,54 @@ function Shop(props: Props) {
     if (url) window.open(url, "_blank");
   };
 
+  const handleSelectInstagram = () => {
+    const url = `https://instagram.com/${props.shop.instagram}`;
+    if (url) window.open(url, "_blank");
+  };
+
   return (
-    <div className="shop" onClick={handleSelectShop}>
-      <h3 className="title is-3">{props.shop.name}</h3>
-      <p>{props.shop.desc}</p>
+    <div className="card shop">
+      <header className="card-header">
+        <p
+          className="shop__title card-header-title is-centered"
+          onClick={handleSelectShop}
+          title={`Click to visit ${props.shop.name}`}
+        >
+          {props.shop.name}
+        </p>
+      </header>
+      <div className="card-content">
+        <div className="content">
+          <p className="shop__desc">{props.shop.desc}</p>
+          {props.shop.instagram && (
+            <p className="shop__instagram" onClick={handleSelectInstagram}>
+              Instagram: @{props.shop.instagram}
+            </p>
+          )}
+          <div className="shop__geo">
+            {props.shop.address && <p>Address: {props.shop.address}</p>}
+            {props.shop.neighbourhood && (
+              <p>Neighbourhood: {props.shop.neighbourhood}</p>
+            )}
+          </div>
+        </div>
+      </div>
+      <footer className="card-footer shop__metadata">
+        {props.shop.hasDelivery && (
+          <p className="card-footer-item">
+            Delivery: Yes
+            {props.shop.hasDelivery === "local" && (
+              <span className="metadata__local"> (local only)</span>
+            )}
+          </p>
+        )}
+        {props.shop.hasPickup && (
+          <p className="card-footer-item">Pickup: Yes</p>
+        )}
+        {props.shop.hasGiftCards && (
+          <p className="card-footer-item">Gift cards: Yes</p>
+        )}
+      </footer>
     </div>
   );
 }
