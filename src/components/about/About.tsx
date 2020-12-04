b37a27a5c3b7018ed3b86666bf6843433309da15
Andy Tran <transient.tran@gmail.com>
2020-12-04 12:22:26 -0500

    fill out About page

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import "./About.scss";
 
 export function About() {
@@ -6,7 +8,7 @@ export function About() {
       <p>
         Rouge is intended to be a directory of local businesses, beginning in
         Toronto (where the creator resides). The future goal is to make it as
-        easy to search through and use as a certain global online marketplace.
+        easy to use as a certain global online marketplace.
       </p>
       <p>
         I'd like to credit all the people who posted on this Reddit thread for
@@ -19,6 +21,16 @@ export function About() {
         >
           https://www.reddit.com/r/askTO/comments/jzrxef/megathread_support_independent_local_businesses/
         </a>
+      </p>
+      <p>
+        If you see any errors in a shop description, please click on the three
+        dots in the top right corner of the card to submit suggestions and I'll
+        look into it asap.
+      </p>
+      <p>
+        I would love to hear from you on what would make this app easier and
+        more convenient to use. You can get in touch using the{" "}
+        <Link to="/Contact">Contact</Link> form on this site.
       </p>
     </div>
   );
