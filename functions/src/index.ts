7002a1cb6f0db2b1823be45dc0e9aa53e67ebecc
Andy Tran <transient.tran@gmail.com>
2020-12-03 20:16:23 -0500

    fill body of email with data

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,8 @@ import * as functions from "firebase-functions";
 const admin = require("firebase-admin");
 const nodemailer = require("nodemailer");
 const cors = require("cors")({ origin: true });
+import { IShop } from "./shop.model";
+
 admin.initializeApp();
 
 let transporter = nodemailer.createTransport({
@@ -17,14 +19,28 @@ let transporter = nodemailer.createTransport({
 export const sendMail = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
     const dest = req.query.dest;
-    // const shop = req.body.shop;
-    // const content = `Content: ${shop}`;
+    const shop: IShop = req.body;
+    const content = `
+    <div>
+      <p>A submission was made on Rouge App:</p>
+      Id: ${shop.id}<br />
+      Name: ${shop.name}<br />
+      Tags: ${shop.tags.join(",")}<br />
+      URL: ${shop.url}<br />
+      Instagram: ${shop.instagram}<br />
+      Description: ${shop.desc}<br />
+      Address: ${shop.address}<br />
+      Neighbourhood: ${shop.neighbourhood}<br />
+      hasDelivery: ${shop.hasDelivery}<br />
+      hasPickup: ${shop.hasPickup}<br />
+      hasGiftCards: ${shop.hasGiftCards}
+    </div>`;
 
     const mailOptions = {
       from: "Rouge App <transient.tran@gmail.com>",
       to: dest,
       subject: "Rouge App Submission",
-      html: "<div>Test</div>",
+      html: content,
     };
 
     return transporter.sendMail(mailOptions, (err: any, info: any) => {
